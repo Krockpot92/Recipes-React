c2f067679b639eaf058b7248e35599adebd2f996
Krockpot92 <89946864+Krockpot92@users.noreply.github.com>
2021-12-17 14:02:44 -0500

    Update RecipeList.js

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -10,7 +10,7 @@ function RecipeList({recipes,setRecipes}) {
       <tr key={index}>
         <td>{recipe.name}</td>
         <td>{recipe.cuisine}</td>
-        <td><img src={recipe.photo}/></td>
+        <td><img src={recipe.photo} alt="food"/></td>
         <td className="content_td"><p>{recipe.ingredients}</p></td>
         <td className="content_td"><p>{recipe.preparation}</p></td>
         <td><button name="delete" onClick={()=> handleRemove(index)}>Delete</button></td>
