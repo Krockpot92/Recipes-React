6e57159f5754c21e58df8449bb5b39ecf678e51e
Krockpot92 <89946864+Krockpot92@users.noreply.github.com>
2021-12-23 21:13:09 -0500

    Add files via upload

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,32 +1,27 @@
 import React from "react";
+import Recipe from "./Recipe";
 
 function RecipeList({recipes,setRecipes}) {
-  
+
+  let arr = [...recipes]
+
+  function handleRemove(index){
+    if(index !== -1){
+      arr.splice(index,1)
+      setRecipes(arr)
+    }
+  }
+
   // TODO: Display the list of recipes using the structure of table that is provided.
   // TODO: Create at least one additional component that is used by this component.
   // TODO: Each recipe row must have a delete button - <button name="delete">Delete</button> - that deletes the post when clicked
-  const recipeList = recipes.map((recipe,index)=>{
+  const recipeList = recipes.map((recipe,index)=>{   
     return(
       <tr key={index}>
-        <td>{recipe.name}</td>
-        <td>{recipe.cuisine}</td>
-        <td><img src={recipe.photo} alt="food"/></td>
-        <td className="content_td"><p>{recipe.ingredients}</p></td>
-        <td className="content_td"><p>{recipe.preparation}</p></td>
-        <td><button name="delete" onClick={()=> handleRemove(index)}>Delete</button></td>
+        <Recipe recipe={recipe} index={index} handleRemove={handleRemove}/>
       </tr>
     )
   })
-
-  let arr = [...recipes]
-   
-   function handleRemove(index){
-     if(index !== -1){
-       arr.splice(index,1)
-       setRecipes(arr)
-     }
-   }
-
 
   return (
     <div className="recipe-list">
